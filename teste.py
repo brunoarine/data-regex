8e2b25173e2953a4cb44cc8a643d221d20f0c6ef
soldeace <barine@runbox.com>
2014-02-28 13:47:24 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 16:01:34 2014
+
+@author: bruno.arine
+"""
+
+import re
+
+patternH = "\s(?:as|a)\s(uma|duas|[0-9]+)(?:\shora[s?]|h|\:|\se)\s*(meia|\d*)\s*(da tarde|da manha)?"
+patternD = "\s(?:dia|em)\s(\d+)(?:\sde\s)?(janeiro|fevereiro|marco|abril|maio|junho|julho|agosto|setembro|outubro|novembro|dezembro|\d+)?(?:\sde\s)?(\d{2,4})?"
+patternO = "(?<=^proxim|que vem$).+(hora|dia util|dia|fim de semana|semana|mes|ano|segunda-feira|terca-feira|quarta-feira|quinta-feira|sexta-feira|sabado|domingo).+(que vem)?"
+
+lista = ["fazer almoco as 3h da manha do dia 29 de dezembro",
+         "levar lista de exercicio para o beto dia 28",
+         "reuniao dia 25 as 2:30h",
+         "encontro com a ana as 8 e meia dia 2 de outubro de 98",
+         "concerto as 8 horas dia 7",
+         "levar os 2 homens pra cadeia dia 5 de abril as 8 e 12",
+         "levar o cachorro pra passear a uma e meia da manha",
+         "sair com a ana dia 29 de agosto de 2015 as 9 e 50 da manha",
+         "acender 7 velas a uma e meia da manha",
+         "tomar banho sabado que vem"]
+         
+for item in lista:
+    print item    
+    print "H:", re.findall(patternH,item)
+    print "D:", re.findall(patternD,item)
+  #  print "O:", re.findall(patternO,item)
+    print ""
